9b1336defc77af734b1d9ad0c32acab1a91cd7d0
Akas Bagus Setiawan <146935384+AkasSakti@users.noreply.github.com>
2024-05-20 10:07:53 +0700

    pregreplace.php

--- pregreplace.php
+++ pregreplace.php
@@ -1,5 +1,5 @@
 <?php 
 $text = "Kain batik berasal dari negara Malaysia."; 
-preg_replace("/Malaysia/","Indonesia",$text); 
+$text = preg_replace("/Malaysia\./", "Indonesia", $text); 
 echo $text; 
 ?>
